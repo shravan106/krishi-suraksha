5300e1242ecec8a4663d3bb99983ce8da86c70a6
Shravan Kasturi <shravankasturi12@gmail.com>
2026-04-01 18:06:43 +0530

    everything done

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -38,7 +38,10 @@ console.log("Connected to MySQL");
 /* ================= MIDDLEWARE ================= */
 
 app.use(cors({
-  origin: "*",
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500"
+  ],
   credentials: true
 }));
 
