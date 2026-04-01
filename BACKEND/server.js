df309ae50e42cdbe3482ed5e0d4e7d470df1610f
Shravan Kasturi <shravankasturi12@gmail.com>
2026-04-01 17:15:37 +0530

    connected railway db

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -20,11 +20,13 @@ app.listen(PORT, () => {
 /* ================= DATABASE ================= */
 
 const db = await mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "pass",
-  database: "krishisuraksha",
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 /* ================= MIDDLEWARE ================= */
